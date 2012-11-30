67abbb9644a01cd70fe6915ad85970d21ed9dae1
Ian Epperson <ian@epperson.com>
2012-11-30 10:18:12 -0800

    Refactored to make it work better with customized Telnet handlers.
    Still needs a bit of work to finish it up as a library.

--- telnetsrv/paramiko_ssh.py
+++ telnetsrv/paramiko_ssh.py
@@ -1,106 +1,227 @@
-from binascii import hexlify
-
-from gevent.event import Event
-import gevent.server
 from gevent import monkey; monkey.patch_all()
+
+from binascii import hexlify
+from threading import Event, Thread
+from SocketServer import BaseRequestHandler
+
+#from gevent.event import Event
+import gevent
+import gevent.server
 import paramiko
+from paramiko import Transport, ServerInterface, RSAKey, DSSKey, SSHException, \
+                    AUTH_SUCCESSFUL, AUTH_FAILED, \
+                    OPEN_SUCCEEDED, OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED, \
+                    OPEN_FAILED_UNKNOWN_CHANNEL_TYPE, OPEN_FAILED_RESOURCE_SHORTAGE
 
 from green import TelnetHandler
 
 # Grab the key from the keyfile
-host_key = paramiko.RSAKey(filename='test_rsa.key')
-print 'Read key: ' + hexlify(host_key.get_fingerprint())
+#host_key = paramiko.RSAKey(filename='test_rsa.key')
+#print 'Read key: ' + hexlify(host_key.get_fingerprint())
 
-class Server(paramiko.ServerInterface):
-    def __init__(self):
-        self.shell_request = Event()
+def getRsaKeyFile(filename, password=None):
+    try:
+        key = RSAKey(filename=filename, password=password)
+    except IOError:
+        print 'Generating new server RSA key.'
+        key = RSAKey.generate(1024)
+        key.write_private_key_file(filename, password=password)
+    return key
+
+
+#class Request2Channel(object):
+#    def __init__(self, request, server):
+#        self._orig_request = request
+#        self.client = request._sock
+#                
+#        self.transport = Transport(self.client)
+#        try:
+#            self.transport.load_server_moduli()
+#        except:
+#            print '(Failed to load moduli -- gex will be unsupported.)'
+#            raise
+#        self.transport.add_server_key(host_key)
+#        
+#        try:
+#            self.transport.start_server(server=server)
+#        except paramiko.SSHException, e:
+#           print('SSH negotiation failed. %s' % e)
+#           raise
+#        
+#        self.channel = self.transport.accept(20)
+#        if self.channel is None:
+#            raise RuntimeError('No Channel')
+#        
+#        server.shell_request.wait(10)
+#        if not server.shell_request.isSet():
+#            raise RuntimeError('Client never asked for a shell')
+#        
+#        self._sock = self.channel
+               
+class TelnetToPtyHandler(object):
+    '''Mixin to turn TelnetHandler into PtyHandler'''
+    def __init__(self, *args):
+        super(TelnetToPtyHandler, self).__init__(*args)
+        
+    # Don't mention these, client isn't listening for them.  Blank the dicts.
+    DOACK = {}
+    WILLACK = {}
     
-    def check_channel_request(self, kind, chanid):
-        if kind == 'session':
-            return paramiko.OPEN_SUCCEEDED
-        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+    # Do not ask for auth in the PTY, it'll be handled via SSH, then passed in with the request
+    authCallback = None
 
-    def check_auth_password(self, username, password):
-        print 'check_auth_password(%s, %s)' % (username, password)
-        if (username == 'ian') and (password == 'yo'):
-            return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED
 
-    def check_auth_publickey(self, username, key):
-        print 'Auth attempt with key: ' + hexlify(key.get_fingerprint())
-        #if (username == 'ian') and (key == self.good_pub_key):
-        #    return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED
-
-    def get_allowed_auths(self, username):
-        return 'password,publickey'
-
-    def check_channel_shell_request(self, channel):
-        self.shell_request.set()
-        return True
-
-    def check_channel_pty_request(self, channel, term, width, height, pixelwidth,
-                                  pixelheight, modes):
-        self.term = term
-        print "term: %r, modes: %r" % (term, modes)
-        # modes = http://www.ietf.org/rfc/rfc4254.txt page 18
-        # for i in xrange(50):
-        #    print "%r: %r" % (int(m[i*5].encode('hex'), 16), int(''.join(m[i*5+1:i*5+5]).encode('hex'), 16))
-        return True
-
-class Request2Channel(object):
-    def __init__(self, request):
-        self._orig_request = request
+class SSHHandler(ServerInterface, BaseRequestHandler):
+    telnet_handler = None
+    pty_handler = None
+    host_key = None
+    username = None
+    
+    def __init__(self, request, client_address, server):
+        self.request = request
+        self.client_address = client_address
+        self.tcp_server = server
+        
+        # Keep track of channel information from the transport
+        self.channels = {}
+        
         self.client = request._sock
-                
-        self.transport = paramiko.Transport(self.client)
+        # Transport turns the socket into an SSH transport
+        self.transport = Transport(self.client)
+        
+        # Create the PTY handler class by mixing in
+        class MixedPtyHandler(TelnetToPtyHandler, self.telnet_handler):
+            # BaseRequestHandler does not inherit from object, must call the __init__ directly
+            def __init__(self, *args):
+                TestTelnetHandler.__init__(self, *args)
+        self.pty_handler = MixedPtyHandler
+        
+        
+        # Call the base class to run the handler
+        BaseRequestHandler.__init__(self, request, client_address, server)
+    
+    def setup(self):
+        '''Setup the connection.'''
+        print 'New request from address %s, port %d' % self.client_address
+        
         try:
             self.transport.load_server_moduli()
         except:
             print '(Failed to load moduli -- gex will be unsupported.)'
             raise
-        self.transport.add_server_key(host_key)
-        
-        self.server = Server()
+        try:
+            self.transport.add_server_key(self.host_key)
+        except:
+            if self.host_key is None:
+                raise NotImplementedError('Host key not set!  SSHHandler instance must define the host_key parameter.  Try host_key = paramiko_ssh.getRsaKeyFile("server_rsa.key").')
         
         try:
-            self.transport.start_server(server=self.server)
-        except paramiko.SSHException, e:
+            # Tell transport to use this object as a server
+            print 'Starting SSH server-side negotiation'
+            self.transport.start_server(server=self)
+        except SSHException, e:
            print('SSH negotiation failed. %s' % e)
            raise
         
-        self.channel = self.transport.accept(20)
-        if self.channel is None:
-            raise RuntimeError('No Channel')
+        # Accept any requested channels
+        while True:
+            channel = self.transport.accept(20)
+            if channel is None:
+                # check to see if any thread is running
+                any_running = False
+                for c, thread in self.channels.items():
+                    if thread.is_alive():
+                        any_running = True
+                        break
+                if not any_running:
+                    break
+            else:
+                print 'Accepted channel %s' % channel
+                #raise RuntimeError('No channel requested.')
+
         
-        self.server.shell_request.wait(10)
-        if not self.server.shell_request.isSet():
-            raise RuntimeError('Client never asked for a shell')
+    
+    class dummy_request(object):
+        def __init__(self):
+            self._sock = None
+    
+    @classmethod
+    def streamserver_handle(cls, socket, address):
+        '''Translate this class for use in a StreamServer'''
+        request = cls.dummy_request()
+        request._sock = socket
+        server = None
+        cls(request, address, server)
         
-        self._sock = self.channel
-               
+    
+    def finish(self):
+        '''Called when the socket closes from the client.'''
+        self.transport.close()
+
+    
+    def check_channel_request(self, kind, chanid):
+        if kind == 'session':
+            return OPEN_SUCCEEDED
+        return OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+
+    def check_auth_password(self, username, password):
+        print 'check_auth_password(%s, %s)' % (username, password)
+        if (username == 'ian') and (password == 'yo'):
+            return AUTH_SUCCESSFUL
+        return AUTH_FAILED
+
+    def check_auth_publickey(self, username, key):
+        print 'Auth attempt with key: ' + hexlify(key.get_fingerprint())
+        #if (username == 'ian') and (key == self.good_pub_key):
+        #    return paramiko.AUTH_SUCCESSFUL
+        return AUTH_FAILED
+
+    def get_allowed_auths(self, username):
+        return 'password,publickey'
+
+    def check_channel_shell_request(self, channel):
+        '''Request to start a shell on the given channel'''
+        try:
+            self.channels[channel].start()
+        except KeyError:
+            print 'Error, requested to start a channel that was not set up.'
+            return False
+        else:
+            return True
+
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth,
+                                  pixelheight, modes):
+        '''Request to allocate a PTY terminal.'''
+        #self.sshterm = term
+        #print "term: %r, modes: %r" % (term, modes)
+        pty_thread = Thread( target=self.start_pty_request, args=(channel, term, modes) )
+        self.channels[channel] = pty_thread
+        
+        return True
+
+    def start_pty_request(self, channel, term, modes):
+        '''Start a PTY - intended to run it a (green)thread.'''
+        request = self.dummy_request()
+        request._sock = channel
+        request.modes = modes
+        request.term = term
+        request.username = self.username
+
+        # modes = http://www.ietf.org/rfc/rfc4254.txt page 18
+        # for i in xrange(50):
+        #    print "%r: %r" % (int(m[i*5].encode('hex'), 16), int(''.join(m[i*5+1:i*5+5]).encode('hex'), 16))
+
+        
+        # This should block until the user quits the pty
+        self.pty_handler(request, self.client_address, self.tcp_server)
+        
+        # Shutdown the entire session
+        self.transport.close()
+        
+        
 
 
-class SSHHandler(TelnetHandler):
-    def setup(self):
-        "Connect incoming connection to a telnet session"
-        
-        # The connected socket should be an SSH transport,
-        # not to be treated like a raw socket.
-        # So, pull out the original request and replace with 
-        # what the rest of the handler will expect: a socket to read/write
-        
-        self.request = Request2Channel(self.request)
-        self.sock = self.request._sock
-        self.setterm(self.request.server.term)
-        
-        # Don't mention these, client isn't listening for them.  Blank the dicts.
-        self.DOACK = {}
-        self.WILLACK = {}
-        
-        # Call the base class setup
-        TelnetHandler.setup(self)
- 
+class TestTelnetHandler(TelnetHandler): 
     def cmdTERM(self, params):
         '''
         Hidden command to print the current TERM
@@ -108,7 +229,13 @@ class SSHHandler(TelnetHandler):
         '''
         self.writeresponse( self.TERM )
 
-        
-print 'Starting server...'        
-server = gevent.server.StreamServer(('', 10444), SSHHandler.streamserver_handle)
+
+class TestSSHHandler(SSHHandler):
+    telnet_handler = TestTelnetHandler
+    host_key = getRsaKeyFile("server_rsa.key")
+
+
+print 'Starting server with %r ...' % TestSSHHandler.streamserver_handle
+#TestSSHHandler.streamserver_handle(None, None)
+server = gevent.server.StreamServer(('', 10444), TestSSHHandler.streamserver_handle)
 server.serve_forever()
